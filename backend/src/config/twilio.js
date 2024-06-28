bab334be1c21cec36dc7f560c9a1771bca7540c9
adamnomani <anomani@seas.upenn.edu>
2024-06-28 12:16:42 -0400

    prep for uzi

--- backend/src/config/twilio.js
+++ backend/src/config/twilio.js
@@ -128,6 +128,6 @@ async function handleIncomingMessage(req, res) {
 module.exports = {
   sendMessage,
   handleIncomingMessage,
-  sendMessages,
-  sendNotificationToUser
+  sendMessages
+  // sendNotificationToUser
 };
