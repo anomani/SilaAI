b17401a3788f88a46fb7378daca7a32fa0b813e3
adamnomani <anomani@seas.upenn.edu>
2024-07-18 16:41:52 -0400

    for uzi

--- backend/src/model/appointment.js
+++ backend/src/model/appointment.js
@@ -145,16 +145,18 @@ async function findAndUpdateAppointmentByAcuityId(acuityId, updateData) {
 }
 
 async function getUpcomingAppointments(clientId, limit = 5) {
+    const db = dbUtils.getDB();
   const currentDate = new Date().toISOString().split('T')[0];
   const query = `
     SELECT * FROM Appointment
-    WHERE clientId = $1 AND date >= $2
-    ORDER BY date ASC, startTime ASC
+    WHERE clientid = $1 AND date >= $2
+    ORDER BY date ASC, starttime ASC
     LIMIT $3
   `;
   const res = await db.query(query, [clientId, currentDate, limit]);
   return res.rows;
 }
+
 
 module.exports = {
     createAppointment,
