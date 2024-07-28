0a6f1e17b7ec6d12fada468e27220212de28a9ab
adamnomani <anomani@seas.upenn.edu>
2024-07-28 18:06:16 -0400

    for uzi

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,8 @@
-require('dotenv').config();
-
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');  // Import body-parser
+const bodyParser = require('body-parser');
 const followupRoutes = require('./routes/followupRoutes');
-const chatRoutes = require('./routes/chatRoutes');  // Import chat routes
+const chatRoutes = require('./routes/chatRoutes');
 const appointmentRoutes = require('./routes/appointmentRoutes');
 const clientRoutes = require('./routes/clientRoutes');
 const tokenRoutes = require('./routes/tokenRoutes');
@@ -14,8 +12,10 @@ const noteRoutes = require('./routes/notesRoutes');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
-app.use(bodyParser.json()); // Middleware to parse JSON bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Uzi Barber App"));
 
 // Routes
 app.use('/api/followup', followupRoutes);
@@ -26,28 +26,5 @@ app.use('/api', tokenRoutes);
 app.use('/webhook', webhookRoutes);
 app.use('/api/ai-prompt', aiPromptRoutes);
 app.use('/api/notes', noteRoutes);
-
-const { connectRedis } = require('./config/redis');
-
-const port = process.env.PORT || 3000;
-
-async function startServer() {
-  try {
-    // Wait for Redis to connect
-    await connectRedis();
-    console.log('Connected to Redis');
-
-    // Start your Express server
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
-  } catch (error) {
-    console.error('Failed to connect to Redis:', error);
-    console.error('Redis URL:', process.env.REDIS_URL);
-    process.exit(1);
-  }
-}
-
-startServer();
 
 module.exports = app;
