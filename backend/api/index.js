2d26889fa61f46307697dd1bd5d0acb3712c775d
adamnomani <anomani@seas.upenn.edu>
2024-07-28 20:12:47 -0400

    for uzi

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,24 +1,12 @@
-require('dotenv').config();
 const app = require('../src/app');
-const { connectRedis } = require('../src/config/redis');
-
 const port = process.env.PORT || 3000;
 
-async function startServer() {
-  try {
-    // Wait for Redis to connect
-    await connectRedis();
-    console.log('Connected to Redis');
+const bodyParser = require('body-parser');
+app.get("/", (req, res) => res.send("Uzi Barber App"));
 
-    // Start your Express server
-    app.listen(port, () => {
-      console.log(`Server is running at http://localhost:${port}`);
-    });
-  } catch (error) {
-    console.error('Failed to connect to Redis:', error);
-    console.error('Redis URL:', process.env.REDIS_URL);
-    process.exit(1);
-  }
-}
+app.use(bodyParser.json());
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
 
-startServer();
+module.exports = app;
--- backend/src/config/queueConfig.js
+++
@@ -1,5 +0,0 @@
-const Queue = require('bull');
-
-const messageQueue = new Queue('message-queue', process.env.REDIS_URL);
-
-module.exports = { messageQueue };
