3a618df01efc7135f0ba01cc05aacc74b663fdcd
adamnomani <anomani@seas.upenn.edu>
2024-07-29 14:24:33 -0400

    for uzi

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { handleChatRequest, handleUserInputDataController, getMessagesByClientIdController, getAllMessagesGroupedByClient, sendMessageController, setMessagesReadController, getCustomListController, sendMessagesToSelectedClients } = require('../controllers/chatController');
+const { handleChatRequest, handleUserInputDataController, getMessagesByClientIdController, getAllMessagesGroupedByClientController, sendMessageController, setMessagesReadController, getCustomListController, sendMessagesToSelectedClients } = require('../controllers/chatController');
 const { handleIncomingMessage } = require('../config/twilio');
 
 router.post('/schedule', handleChatRequest);
 router.post('/incoming', handleIncomingMessage);
 router.post('/handle-user-input', handleUserInputDataController);
 router.get('/messages/:clientId', getMessagesByClientIdController);
-router.get('/messages', getAllMessagesGroupedByClient);
+router.get('/messages', getAllMessagesGroupedByClientController);
 router.post('/send-message', sendMessageController);
 router.post('/set-messages-read/:clientId', setMessagesReadController);
 router.get('/custom-list', getCustomListController);
