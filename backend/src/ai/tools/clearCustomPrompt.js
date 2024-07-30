7726d3377291744fe5f68f9d8a939fddd762aed8
adamnomani <anomani@seas.upenn.edu>
2024-07-30 18:53:08 -0400

    for uzi

--- backend/src/ai/tools/clearCustomPrompt.js
+++ backend/src/ai/tools/clearCustomPrompt.js
@@ -5,8 +5,5 @@ async function clearCustomPrompt(clientId) {
     await deleteAIPrompt(clientId)
 }
 
-async function main() {
-    await clearCustomPrompt(3367)
-}
-main()
+
 module.exports = {clearCustomPrompt};
