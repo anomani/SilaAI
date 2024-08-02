3b7cbeeea4490503a80c36e9515d5e376fc0032e
adamnomani <anomani@seas.upenn.edu>
2024-08-02 18:01:19 -0400

    for uzi

--- backend/src/ai/scheduling.js
+++ backend/src/ai/scheduling.js
@@ -382,7 +382,7 @@ tempTools = [{
 }]
 async function createTemporaryAssistant(phoneNumber) {
   const newAssistant = await openai.beta.assistants.create({
-    instructions: "Initially say hey bro, don't think I've heard from you before. Can you just give me your first and last name so I can save it? Then call the createClient tool with the corresponding first name and last name. After you call it respond by saying, How can I help you today bro",
+    instructions: "Initially say hey bro, don't think I've heard from you before. Can you just give me your first and last name so I can save it? Then with the name that they give call the createClient tool with the corresponding first name and last name. After you call it respond by saying, How can I help you today bro",
     name: `Assistant to get name of the client`,
     model: "gpt-4o",
     tools: [...tempTools, ...tools],
@@ -392,6 +392,7 @@ async function createTemporaryAssistant(phoneNumber) {
 }
 
 async function handleToolCalls(requiredActions, client) {
+  console.log(requiredActions)
   const toolOutputs = [];
 
   for (const action of requiredActions.tool_calls) {
