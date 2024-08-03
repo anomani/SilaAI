f745c11a607b5c3016485a339681ce113c905f6b
adamnomani <anomani@seas.upenn.edu>
2024-08-03 13:13:44 -0400

    for uzi

--- backend/src/ai/tools/bookAppointment.js
+++ backend/src/ai/tools/bookAppointment.js
@@ -148,19 +148,19 @@ async function bookAppointment(date, startTime, fname, lname, phone, email, appo
     }
 
     try {
-        const acuityAppointment = await bookAppointmentWithAcuity(date, startTime, fname, lname, phone, email, appointmentType, price, addOnArray);
-        console.log(acuityAppointment);
-        // const client = await getClientByPhoneNumber(phone);
-        // if (client.id != '') {
-        //     console.log("Client already exists");
-        //     const clientId = client.id;
-        //     await createAppointment(appointmentType, acuityAppointment.id, date, startTime, endTime, clientId, "", price);
-        // } else {
-        //     console.log("Client does not exist");
-        //     await createClient(fname, lname, phone, email, "");
-        //     const newClient = await getClientByPhoneNumber(phone);
-        //     await createAppointment(appointmentType, acuityAppointment.id, date, startTime, endTime, newClient.id, "", price);
-        // }
+        // const acuityAppointment = await bookAppointmentWithAcuity(date, startTime, fname, lname, phone, email, appointmentType, price, addOnArray);
+        // console.log(acuityAppointment);
+        const client = await getClientByPhoneNumber(phone);
+        if (client.id != '') {
+            console.log("Client already exists");
+            const clientId = client.id;
+            await createAppointment(appointmentType, 123, date, startTime, endTime, clientId, "", price);
+        } else {
+            console.log("Client does not exist");
+            await createClient(fname, lname, phone, email, "");
+            const newClient = await getClientByPhoneNumber(phone);
+            await createAppointment(appointmentType, 123, date, startTime, endTime, newClient.id, "", price);
+        }
         return "Appointment booked successfully";
     } catch (error) {
         console.log(error);
