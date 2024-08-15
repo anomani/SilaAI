12597503875a075069cc4a55bf2ea887bd367eff
adamnomani <anomani@seas.upenn.edu>
2024-08-15 13:59:36 -0400

    for uzi

--- backend/src/controllers/followupController.js
+++ backend/src/controllers/followupController.js
@@ -7,41 +7,41 @@ const fs = require('fs');
 
 
 async function fetchClients(req, res) {
-    try {
-      const minDaysSinceLast = 0; 
-      const filePath = path.resolve(__dirname, '../../data/list.csv');
-      const clients = await readCSV(filePath, minDaysSinceLast);
-      res.status(200).json(clients);
-    } catch (error) {
-      res.status(500).send(`Error fetching clients: ${error.message}`);
-    }
+    // try {
+    //   const minDaysSinceLast = 0; 
+    //   const filePath = path.resolve(__dirname, '../../data/list.csv');
+    //   const clients = await readCSV(filePath, minDaysSinceLast);
+    //   res.status(200).json(clients);
+    // } catch (error) {
+    //   res.status(500).send(`Error fetching clients: ${error.message}`);
+    // }
   }
 
   async function sendFollowUpMessages(req, res) {
-    try {
-        const selectedClients = req.body.clients; // Expect an array of selected clients
+    // try {
+    //     const selectedClients = req.body.clients; // Expect an array of selected clients
 
-        const messages = selectedClients.map(client => {
-            const messageBody = `Hello ${client['First Name']}, we miss you at the barbershop! It's been a while since your last appointment. Schedule your next visit now!`;
-            return sendMessage(client['Phone'], messageBody);
-        });
+    //     const messages = selectedClients.map(client => {
+    //         const messageBody = `Hello ${client['First Name']}, we miss you at the barbershop! It's been a while since your last appointment. Schedule your next visit now!`;
+    //         return sendMessage(client['Phone'], messageBody);
+    //     });
 
-        await Promise.all(messages);
+    //     await Promise.all(messages);
 
-        res.status(200).send('Messages sent successfully');
-    } catch (error) {
-        res.status(500).send(`Error sending messages: ${error.message}`);
-    }
+    //     res.status(200).send('Messages sent successfully');
+    // } catch (error) {
+    //     res.status(500).send(`Error sending messages: ${error.message}`);
+    // }
 }
 
 async function updateClientData(req, res) {
-    try {
-        await getClients();
-        await getCSV();
-        res.status(200).send('Client data updated successfully');
-    } catch (error) {
-        res.status(500).send(`Error updating client data: ${error.message}`);
-    }
+    // try {
+    //     await getClients();
+    //     await getCSV();
+    //     res.status(200).send('Client data updated successfully');
+    // } catch (error) {
+    //     res.status(500).send(`Error updating client data: ${error.message}`);
+    // }
 }
 
 
