965e8055907ebfd6f425276e19c498d01fc21109
adamnomani <anomani@seas.upenn.edu>
2024-10-06 23:21:26 -0400

    for uzi

--- backend/src/routes/tokenRoutes.js
+++ backend/src/routes/tokenRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { savePushTokenController } = require('../controllers/tokenController');
-const { authenticateToken } = require('../middleware/auth');
+const { authenticateToken } = require('../middleware/authMiddleware');
 
 router.post('/save-push-token', authenticateToken, savePushTokenController);
 
