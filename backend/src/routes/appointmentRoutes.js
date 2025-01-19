4ea0db4b153cc87aeb38958d54b36d8f9a3e8102
adamnomani <anomani@seas.upenn.edu>
2025-01-18 22:15:47 -0500

    for uzi

--- backend/src/routes/appointmentRoutes.js
+++ backend/src/routes/appointmentRoutes.js
@@ -29,21 +29,21 @@ router.put('/appointments/appointment-types/:appointmentTypeId', authenticateTok
 router.get('/compatible-addons', authenticateToken, getCompatibleAddOnsController);
 
 
-router.get('/appointments/:date', authenticateToken, getAppointmentsByDate);
-router.post('/appointments', authenticateToken, createNewAppointment);
-router.get('/appointments/client/:clientId', authenticateToken, getAppointmentsByClientId);
-router.delete('/appointments/:appointmentId', authenticateToken, delAppointment);
-router.post('/appointments/acuity', authenticateToken, bookAppointmentWithAcuityController);
-router.post('/appointments/blocked-time', authenticateToken, createBlockedTimeController);
-router.get('/appointments/client/:clientId/around-current/:currentAppointmentId', authenticateToken, getClientAppointmentsAroundCurrentController);
-router.put('/appointments/:appointmentId/payment', authenticateToken, updateAppointmentPaymentController);
-router.put('/appointments/:appointmentId/reschedule', authenticateToken, rescheduleAppointmentController);
+router.get('/:date', authenticateToken, getAppointmentsByDate);
+router.post('/', authenticateToken, createNewAppointment);
+router.get('/client/:clientId', authenticateToken, getAppointmentsByClientId);
+router.delete('/:appointmentId', authenticateToken, delAppointment);
+router.post('/acuity', authenticateToken, bookAppointmentWithAcuityController);
+router.post('/blocked-time', authenticateToken, createBlockedTimeController);
+router.get('/client/:clientId/around-current/:currentAppointmentId', authenticateToken, getClientAppointmentsAroundCurrentController);
+router.put('/:appointmentId/payment', authenticateToken, updateAppointmentPaymentController);
+router.put('/:appointmentId/reschedule', authenticateToken, rescheduleAppointmentController);
 router.get('/metrics', authenticateToken, getAppointmentMetricsController);
-router.put('/appointments/:appointmentId', authenticateToken, updateAppointmentDetailsController);
+router.put('/:appointmentId', authenticateToken, updateAppointmentDetailsController);
 
 
 //THESE DO NOT REQUIRE AUTHENTICATION
-router.post('/appointments/confirm', confirmAppointment);
+router.post('/confirm', confirmAppointment);
 
 // Update the /availabilities route to use appointmentTypeId
 router.get('/availabilities', getAvailabilities);
